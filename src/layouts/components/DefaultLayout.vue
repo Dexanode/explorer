5e23e754d10c5f12d9f3bdc88d8f917ff566625a
nadi555 <chaeruloemar0309@gmail.com>
2023-06-21 23:16:06 +0700

    first

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -53,7 +53,7 @@ function selected(route: any, nav: NavLink) {
 </script>
 
 <template>
-  <div class="bg-gray-100 dark:bg-[#171d30]">
+  <div class="bg-gray-100 dark:bg-[#0c1121]">
     <!-- sidebar -->
     <div
       class="w-64 fixed z-50 left-0 top-0 bottom-0 overflow-auto bg-base-100 border-r border-gray-100 dark:border-gray-700"
@@ -62,8 +62,8 @@ function selected(route: any, nav: NavLink) {
       <div class="flex justify-between mt-1 pl-4 py-4 mb-1">
         <RouterLink to="/" class="flex items-center">
           <img class="w-10 h-10" src="../../assets/logo.svg" />
-          <h1 class="flex-1 ml-3 text-2xl font-semibold dark:text-white">
-            Ping.pub
+          <h1 class="flex-1 ml-3 text-2xl font-semibold dark:text-teal-200">
+            DEXANODE EXPLORER
           </h1>
         </RouterLink>
         <div
